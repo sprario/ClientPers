2ef90a4890b6316ee2c46d86ab20f6f1c2ec2d27
Santiago Prario <sprario@persoft-integraciones.com>
2022-12-15 11:01:38 -0300

    feat: :fire: merge branchs

--- src/hooks/useQuery/index.ts
+++ src/hooks/useQuery/index.ts
@@ -1,66 +1,65 @@
-import { useContext, useEffect, useMemo } from 'react';
-import {
-	useQuery as useQueryTanStack,
-	UseQueryOptions as UseQueryOptionsTanStack,
-	UseQueryResult,
-} from 'react-query';
+// import { useContext, useEffect, useMemo } from 'react';
+// import {
+// 	useQuery as useQueryTanStack,
+// 	UseQueryOptions as UseQueryOptionsTanStack,
+// 	UseQueryResult,
+// } from 'react-query';
 
-import { FetchServiceEndpoint, FetchError } from '../../types/service';
-import { AppContext, AuthContext } from '../../context';
-import { AppError } from '../../types/core';
+// import { FetchServiceEndpoint, FetchError } from '../../types/service';
+// import { AppContext, AuthContext } from '../../context';
+// import { AppError } from '../../types/core';
 
 
-interface UseQueryOptionsProps<T> extends Omit<UseQueryOptionsTanStack<T, FetchError<T>>, 'onSettled'> {
-  showLoadingBackdrop?: boolean;
-  handleErrors?: boolean;
-}
+// interface UseQueryOptionsProps<T> extends Omit<UseQueryOptionsTanStack<T, FetchError<T>>, 'onSettled'> {
+//   showLoadingBackdrop?: boolean;
+//   handleErrors?: boolean;
+// }
 
-function useQuery<T, P>(
-  fetch: (params?: P) => FetchServiceEndpoint<T>,
-  config?: UseQueryOptionsProps<T>,
-): UseQueryResult<T, FetchError<T>> {
-  const { accessToken } = useContext(AuthContext);
-  const { setLoading, setError } = useContext(AppContext);
-  const { keys, fetcher } = useMemo(() => {
-    return fetch();
-  }, [fetch]);
+// function useQuery<T, P>(
+//   fetch: (params?: P) => FetchServiceEndpoint<T>,
+//   config?: UseQueryOptionsProps<T>,
+// ): UseQueryResult<T, FetchError<T>> {
+//   const { accessToken } = useContext(AuthContext);
+//   const { setLoading, setError } = useContext(AppContext);
+//   const { keys, fetcher } = useMemo(() => {
+//     return fetch();
+//   }, [fetch]);
 
-  const { showLoadingBackdrop, handleErrors, ...useQueryConfig } = useMemo(
-    () => ({
-      showLoadingBackdrop: false,
-      handleErrors: true,
-      onSettled: () => {
-        if (showLoadingBackdrop) {
-          setLoading(false);
-        }
-      },
-      ...config,
-    }),
-    [config, setLoading]
-  );
+//   const { showLoadingBackdrop, handleErrors, ...useQueryConfig } = useMemo(
+//     () => ({
+//       showLoadingBackdrop: false,
+//       handleErrors: true,
+//       onSettled: () => {
+//         if (showLoadingBackdrop) {
+//           setLoading(false);
+//         }
+//       },
+//       ...config,
+//     }),
+//     [config, setLoading]
+//   );
 
-  const useQueryResult = useQueryTanStack(
-    keys,
-    () => {
-      if(showLoadingBackdrop && useQueryResult.failureCount === 0) {
-        setLoading(true);
-      }
-      return fetcher({ accessToken });
-    },
-    useQueryConfig,
-  );
+//   const useQueryResult = useQueryTanStack(
+//     keys,
+//     () => {
+//       if(showLoadingBackdrop && useQueryResult.failureCount === 0) {
+//         setLoading(true);
+//       }
+//       return fetcher({ accessToken });
+//     },
+//     useQueryConfig,
+//   );
 
-  useEffect(() => {
-    if (handleErrors && useQueryResult.errorUpdatedAt) {
-      if (useQueryResult.error) {
-        setError({ fetchError: useQueryResult.error });
-      } else {
-        setError({} as AppError);
-      }
-    }
-  }, [useQueryResult.error, useQueryResult.errorUpdatedAt, useQueryResult.refetch, handleErrors, setError]);
+//   useEffect(() => {
+//     if (handleErrors && useQueryResult.errorUpdatedAt) {
+//       if (useQueryResult.error) {
+//         setError({ fetchError: useQueryResult.error });
+//       } else {
+//         setError({} as AppError);
+//       }
+//     }
+//   }, [useQueryResult.error, useQueryResult.errorUpdatedAt, useQueryResult.refetch, handleErrors, setError]);
 
-  return useQueryResult;
-}
-
-export default useQuery;
+// export default useQuery;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/explicit-module-boundary-types
+export const test = () => null;
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -22,10 +22,25 @@ const Login: FunctionComponent = () => {
 
 	const navigate = useNavigate();
 
-  const createLoginMutation = useMutation(loginRequest, {
-    showLoadingBackdrop: true,
-  });
+    const createLoginMutation = useMutation(loginRequest, {
+      showLoadingBackdrop: true,
+    });
+	
 	const isDisabled = useMemo(() => !!email && !!password, [email, password]);
+
+	const onSubmit = handleSubmit(() => {
+		createLoginMutation
+			.mutateAsync({ email, password })
+			.then(response => {
+				const { accessToken, userProfile } = response;
+				localStorage.setItem('accessToken', accessToken);
+				setUserProfile(userProfile);
+				navigate('/dashboard');
+			})
+			.catch(err => {
+				setError(err);
+			});
+	});
 
   
   return (
@@ -43,37 +58,14 @@ const Login: FunctionComponent = () => {
                 className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0"
               >
                 <div className="rounded-t mb-0 px-6 py-6">
-                  <div className="text-center mb-3">
-                    <h6 className="text-blueGray-500 text-sm font-bold">
-                      Sign in with
-                    </h6>
-                  </div>
                   <div className="btn-wrapper text-center">
-                    <button
-                      className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center text-xs ease-linear transition-all duration-150"
-                      type="button"
-                    >
-                      <img
-                        alt="..."
-                        className="w-5 mr-1"
-                        src="../../assets/img/github.svg"
-                      />Github</button
-                    ><button
-                      className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center text-xs ease-linear transition-all duration-150"
-                      type="button"
-                    >
-                      <img
-                        alt="..."
-                        className="w-5 mr-1"
-                        src="../../assets/img/google.svg"
-                      />Google
-                    </button>
+					<img src={persoft} alt='logo' />
                   </div>
                   <hr className="mt-6 border-b-1 border-blueGray-300" />
                 </div>
                 <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
                   <div className="text-blueGray-400 text-center mb-3 font-bold">
-                    <small>Or sign in with credentials</small>
+                    <small>Iniciar Sesión</small>
                   </div>
                   <form onSubmit={onSubmit}>
                     <div className="relative w-full mb-3">
@@ -92,8 +84,9 @@ const Login: FunctionComponent = () => {
                       <label
                         className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                         // for="grid-password"
-                        >Password</label
-                      ><input
+                        >Password
+						</label>
+						<input
                         type="password"
                         {...register("password")}
                         className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
