4b3fc7136d0988862ef7ffdff3a4d0f77b2b88cf
Santiago Prario <sprario@persoft-integraciones.com>
2022-12-20 19:03:41 -0300

    feat: :memo: add readme

--- src/pages/Forms/components/FieldContainer/index.tsx
+++ src/pages/Forms/components/FieldContainer/index.tsx
@@ -1,12 +1,40 @@
 import React, { FunctionComponent } from 'react';
+import { formatLongDateHour } from '@/utils/formatters/date';
 
 interface FieldContainerProps {
   label: string;
   value: string;
+  type: string;
 }
 
-const FieldContainer: FunctionComponent<FieldContainerProps> = ({ label, value }: FieldContainerProps ) => {
+const FieldContainer: FunctionComponent<FieldContainerProps> = ({ label, value, type = 'text' }: FieldContainerProps ) => {
 
+  // const text = () => (
+  //   <>
+  //     <div className='flex items-center'>
+  //       <p className='md:min-w-full border-red-600 text-blueGray-500 text-xs uppercase font-bold no-underline'>
+  //         {label + ':'}
+  //       </p>
+  //     </div><div className='border-red-700 px-5 items-center'>
+  //       <p className='text-sm'>{value}</p>
+  //     </div>
+  //   </>
+  // );
+
+  // const date = () => (
+  //   <>
+  //     <div className='flex items-center'>
+  //       <p className='md:min-w-full border-red-600 text-blueGray-500 text-xs uppercase font-bold no-underline'>
+  //         {label + ':'}
+  //       </p>
+  //     </div><div className='border-red-700 px-5 items-center'>
+  //       <p className='text-sm'>{formatLongDateHour(new Date(value))}</p>
+  //     </div>
+  //   </>
+  // );
+  
+  
+  
   return (
     <div
     key={label}
--- src/pages/Forms/components/FormContainer/index.tsx
+++ src/pages/Forms/components/FormContainer/index.tsx
@@ -19,15 +19,17 @@ const FormContainer: FunctionComponent =  () => {
 		<div className='flex flex-col w-full shadow border-l-stone-800 px-4' >
 			{isLoading && <Spinner isfullPage={false} loading={true} size='small' />}
 			<div className=''>
-			  <h2 className='text-blueGray-500 text-xs uppercase font-bold block pt-1 no-underline'>
+			  <h2 className='text-blueGray-500 text-md uppercase font-bold block pt-1 no-underline'>
 					{data ? data.name : ''}
 				</h2>
-			  <h4>ID : {id}</h4>
+			  <h4 className='text-blueGray-500 text-sm uppercase font-bold block pt-1 no-underline'>
+					ID : {id}
+				</h4>
 			</div>
 			<div className='w-full'>
 			{data &&
 				data.fields.map(field => (
-				  <FieldContainer label={field.label} value={field.value} />
+				  <FieldContainer type={field.type} label={field.label} value={field.value} />
 				))}
 			</div>
 		</div>
